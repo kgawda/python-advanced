56f2f2e971b0c0d42587bbfa36c3a3cb47333176
Konrad Gawda <k.gawda@alx.pl>
2025-02-01 15:50:13 +0100

    Add threading

--- game/multi.py
+++ game/multi.py
@@ -1,5 +1,39 @@
+from queue import Empty
 from main import simulate
+from multiprocessing import Process, Queue, Pool
+from threading import Thread
+
+def f(queue):
+    result = simulate()
+    queue.put(result)
+
+def f2(x):
+    return simulate()
 
 if __name__ == "__main__":
-    new_cards_counter = simulate()
-    print(new_cards_counter)
+    processes = []
+    threds = []
+    q = Queue()
+    for _ in range(5):
+        # p = Process(target=f, args=(q,))
+        # processes.append(p)
+        # p.start()
+        t = Thread(target=f, args=(q,))
+        threds.append(t)
+        t.start()
+    
+    # for p in processes:
+    #     p.join()
+    for t in threds:
+        t.join()
+
+    while True:
+        try:
+            print(q.get(block=False))
+        except Empty:
+            break
+
+    # with Pool(10) as pool:
+    #     results = pool.map(f2, range(10))
+    #     print(results)
+
