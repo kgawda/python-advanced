24e97186ff7e54f7523d70482ece5037da4a7c57
Konrad Gawda <k.gawda@alx.pl>
2025-02-06 12:29:29 +0100

    Args from url

--- webapp/app.py
+++ webapp/app.py
@@ -2,13 +2,37 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+def funny_name(name: str) -> str:
+    return name[::-1].title()
+
+@app.context_processor
+def standard_context():
+    return dict(
+        company_name="Acme INC.",
+        # Funkcje też można tu przekazać
+    )
+
+app.jinja_env.filters["funny_name"] = funny_name
+
 @app.route("/")
 def hello_world():
-    name = "<i>Konrad</i>"
+    name = "Konrad"
+    other_names = ["Ania", "Karol", "Kasia"]
     return render_template(
         "home.html.j2",
         name=name,
-        paragraph_count=4
+        paragraph_count=4,
+        other_names=other_names,
+    )
+
+@app.route("/name/<name>")
+def greetings(name):
+    other_names = ["Ania", "Karol", "Kasia"]
+    return render_template(
+        "home.html.j2",
+        name=name,
+        paragraph_count=4,
+        other_names=other_names,
     )
 
 if __name__ == "__main__":
