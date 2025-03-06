e80852d5952489785b4f59863f574764561971bb
Konrad Gawda <k.gawda@alx.pl>
2025-03-06 13:00:25 +0100

    Wersja z zagadką

--- dash_intro/app.py
+++ dash_intro/app.py
@@ -1,4 +1,6 @@
-from dash import Dash, html, dcc, Input, Output
+from numbers import Number
+from dash import Dash, html, dcc, Input, Output, State, callback_context
+from dash.exceptions import PreventUpdate
 from plotly import graph_objects as go
 
 
@@ -10,13 +12,14 @@ def build_figure(constant=0):
     fig = go.Figure([
         go.Bar(
             y=[1, 2, 3, 4, 5],
-            hovertemplate="TEST<br>Value is <b>%{y}</b>"
+            hovertemplate="TEST<br>Value is <b>%{y}</b>",
         ),
         go.Bar(y=[5+constant, 4+constant, 2+constant, 2+constant, 1+constant],),
         ],
-        layout={"title": "Przykład1"},
+        # layout={"title": "Przykład1"},
     )
-    fig.update_layout({"title": "Przykład"})  # nadpisanie ustawień z fig.layout
+    # fig.update_layout({"title": "Przykład"})  # nadpisanie ustawień z fig.layout
+    fig.update_layout(transition_duration=200)
     return fig
 
 
@@ -27,23 +30,59 @@ app.layout = html.Div([
         0,
         10,
         value=10,
+        step=1,
+        updatemode="drag",
         id="plot-variation-slider",
     ),
+    dcc.Input(
+        type='number',
+        value=10,
+        id="plot-variation-input",
+    ),
+    dcc.Input(id="username"),
+    dcc.Store(
+        id="controls-store"
+    ),
     dcc.Graph(
-        style={"height": "100vh"},
+        style={"height": "80vh"},
         id="main-graph"
     )
 ])
 
 
 @app.callback(
-    Output("main-graph", "figure"),
+    Output("controls-store", "data"),
+    Output("plot-variation-slider", "value"),
+    Output("plot-variation-input", "value"),
     Input("plot-variation-slider", "value"),
+    Input("plot-variation-input", "value"),
     # prevent_initial_call=True,
 )
-def update_plot(slider_value):
-    return build_figure(slider_value)
+def update_controls(slider_value, input_value):
+    if callback_context.triggered_id == "plot-variation-slider":
+        plot_parameter = slider_value
+    elif callback_context.triggered_id == "plot-variation-input":
+        plot_parameter = input_value
+    else:
+        plot_parameter = slider_value
+
+    if not isinstance(plot_parameter, Number):
+        raise PreventUpdate()
+    
+    return plot_parameter, plot_parameter, plot_parameter
+
+
+@app.callback(
+    Output("main-graph", "figure"),
+    Input("controls-store", "data"),
+    State("username", "value"),
+)
+def update_plot(control_data, username):
+    print(repr(username))
+    fig = build_figure(control_data)
+    fig.update_layout({"title": "Plot for " + str(username)}, overwrite="True")
+    return fig
 
 
 if __name__ == "__main__":
-    app.run_server(debug=True, use_reloader=True)
+    app.run_server()
